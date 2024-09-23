065cd29bd77842382735da97b303bf21ed9d7ca4
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2024-09-23 11:08:14 +0330

    feat: Add Simple admin

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+
+from exchange import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    pass
+
+@admin.register(models.Wallet)
+class WalletAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    pass
+
+@admin.register(models.Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
