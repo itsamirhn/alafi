d00961887045ff8f01ac8661b6c523c5abb5f2a6
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2024-09-23 10:48:32 +0330

    feat: Implement Purchase Serializer

--- exchange/abc.py
+++ exchange/abc.py
@@ -3,3 +3,11 @@ from exchange import models
 
 def buy_from_exchange(coin: models.Coin, amount: int) -> bool:  # noqa: ARG001
     return True
+
+
+def get_rate_by_usd(coin: models.Coin) -> int:
+    match coin:
+        case models.Coin.USD:
+            return 1
+        case models.Coin.ABAN:
+            return 4
--- exchange/views/api/v1/purchase.py
+++ exchange/views/api/v1/purchase.py
@@ -1,10 +1,8 @@
 import abc
-from typing import Self
 
-from django.db.models import QuerySet
 from rest_framework import mixins, viewsets
 
-from exchange import authentication, models
+from exchange import authentication
 from exchange.serializers.api.v1 import PurchaseSerializer
 
 
@@ -15,9 +13,3 @@ class PurchaseViewSet(
 ):
     serializer_class = PurchaseSerializer
     authentication_classes = (authentication.Authentication,)
-
-    @abc.abstractmethod
-    def get_user(self: Self) -> models.User: ...
-
-    def get_queryset(self: Self) -> QuerySet[models.Transaction]:
-        return models.Transaction.objects.filter(wallet__user=self.get_user()).select_related("wallet")
